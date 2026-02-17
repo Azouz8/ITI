08e210ce01f8810dafeea717f108205c226657e1
Ali Azouz <aliazouz.808@gmail.com>
2026-02-17 21:59:05 +0200

    Solve Day 3 Task of C# Course

---
+++ C#/Day3/Lab/Employee.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Lab
+{
+    /// <summary>
+    /// ENUM for Gender Choice
+    /// </summary>
+    internal enum Gender
+    {
+        M,
+        F
+    }
+    /// <summary>
+    /// ENUM for Security Previliges Choice
+    /// </summary>
+    internal enum SecurityPrev : byte
+    {
+        guest,
+        developer,
+        secretary,
+        DBA
+    }
+    /// <summary>
+    /// The Employee Class
+    /// </summary>
+    internal struct Employee
+    {
+        int id, salary;
+        Gender gender;
+        SecurityPrev securityPrev;
+        HiringDate hiringDate;
+        /// <summary>
+        /// Parameterized Constructor of the Employee
+        /// </summary>
+        public Employee(int id, int salary, Gender gender, SecurityPrev securityPrev, HiringDate hiringDate)
+        {
+            this.id = id;
+            this.salary = salary;
+            this.gender = gender;
+            this.securityPrev = securityPrev;
+            this.hiringDate = hiringDate;
+        }
+        /// <summary>
+        /// The setters of the fields of the employee
+        /// </summary>
+        public void setID(int id)
+        {
+            this.id = id;
+        }
+        public void setSalary(int salary)
+        {
+            this.salary = salary;
+        }
+        public void setGender(string gender)
+        {
+            try
+            {
+                this.gender = (Gender)Enum.Parse(typeof(Gender), gender, true);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Gender must be 'M' or 'F'.", nameof(gender), ex);
+            }
+        }
+        public void setSecPrev(SecurityPrev securityPrev)
+        {
+            this.securityPrev = securityPrev;
+        }
+        public void setHiringDate(HiringDate hiringDate)
+        {
+            this.hiringDate = hiringDate;
+        }
+        /// <summary>
+        /// The getters of the fields of the employee
+        /// </summary>
+        public int getID()
+        {
+            return id;
+        }
+
+        public int getSalary()
+        {
+            return salary;
+        }
+
+        public string getGender()
+        {
+            return gender.ToString();
+        }
+
+        public string getSecPrev()
+        {
+            return securityPrev.ToString();
+        }
+
+        public String getHiringDate()
+        {
+            return hiringDate.ToString();
+        }
+        public override string ToString()
+        {
+            string message = String.Format("Emp ID: {0}, Salary: {1:C}", id, salary);
+            return message;
+        }
+        /// <summary>
+        /// Sorting method for and array of Employee based on their hiring date
+        /// </summary>
+        public static void SortEmp(ref Employee[] emp)
+        {
+            for (int i = 0; i < emp.Length; i++)
+            {
+                for (int j = i + 1; j < emp.Length; j++)
+                {
+                    if (emp[i].hiringDate.getYear() == emp[j].hiringDate.getYear())
+                    {
+                        if (emp[i].hiringDate.getMonth() == emp[j].hiringDate.getMonth())
+                        {
+                            if (emp[i].hiringDate.getDay() > emp[j].hiringDate.getDay())
+                            {
+                                Employee tempEmp;
+                                tempEmp = emp[i];
+                                emp[i] = emp[j];
+                                emp[j] = tempEmp;
+                            }
+                        }
+                        else if (emp[i].hiringDate.getMonth() > emp[j].hiringDate.getMonth())
+                        {
+                            Employee tempEmp;
+                            tempEmp = emp[i];
+                            emp[i] = emp[j];
+                            emp[j] = tempEmp;
+                        }
+                    }
+                    else if (emp[i].hiringDate.getYear() > emp[j].hiringDate.getYear())
+                    {
+                        Employee tempEmp;
+                        tempEmp = emp[i];
+                        emp[i] = emp[j];
+                        emp[j] = tempEmp;
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ C#/Day3/Lab/Program.cs
@@ -0,0 +1,79 @@
+﻿using Lab;
+
+namespace Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[3];
+            for (int i = 0; i < 3; i++)
+            {
+                string input;
+                int id;
+                while (true)
+                {
+                    Console.WriteLine($"Enter Id for Emp No. {i + 1}");
+                    input = Console.ReadLine();
+                    if (int.TryParse(input, out id))
+                        break;
+
+                    Console.WriteLine("Invalid input! Please enter Numeric Value.");
+                }
+                int salary;
+                while (true)
+                {
+                    Console.WriteLine($"Enter Salary for Emp No. {i + 1}");
+                    input = Console.ReadLine();
+                    if (int.TryParse(input, out salary))
+                        break;
+
+                    Console.WriteLine("Invalid input! Please enter Numeric Value.");
+                }
+
+                Gender gender;
+                while (true)
+                {
+                    Console.WriteLine($"Enter Gender (M/F) for Emp No. {i + 1}");
+                    string genderIn = Console.ReadLine();
+
+                    if (Enum.TryParse(genderIn, true, out gender))
+                        break;
+
+                    Console.WriteLine("Invalid input! Please enter M or F.");
+                }
+                SecurityPrev securityPrev;
+                while (true)
+                {
+                    Console.WriteLine($"Enter Security Level(guest, developer, secretary, DBA) for Emp No. {i + 1}");
+                    string secLevel = Console.ReadLine();
+                    if (Enum.TryParse(secLevel, true, out securityPrev))
+                        break;
+                    Console.WriteLine("Invalid input! Please enter Correct Security Level.");
+                }
+                DateTime date;
+                while (true)
+                {
+                    Console.WriteLine($"Enter Hiring Date(2025/01/01) for Emp No. {i + 1}");
+                    string hiringDate = Console.ReadLine();
+                    if (DateTime.TryParse(hiringDate, out date))
+                        break;
+                    Console.WriteLine("Invalid input! Please enter Date in the correct format.");
+                }
+                employees[i] = new Employee(id, salary, gender, securityPrev, new HiringDate(date.Day, date.Month, date.Year));
+
+            }
+
+            Employee.SortEmp(ref employees);
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"ID: {employees[i].getID()}");
+                Console.WriteLine($"Salary: {employees[i].getSalary()}");
+                Console.WriteLine($"Gender: {employees[i].getGender()}");
+                Console.WriteLine($"Sec Level: {employees[i].getSecPrev()}");
+                Console.WriteLine($"Hire Date: {employees[i].getHiringDate()}");
+
+            }
+        }
+    }
+}
